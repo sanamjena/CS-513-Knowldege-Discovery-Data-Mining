0c04b834f8ba43c8958e72969034fb0461403838
Sanam Sritam Jena <sanamsritam@gmail.com>
2020-02-10 19:26:20 -0500

    Completed homework 1

---
+++ TEST_CODES/test.R
@@ -0,0 +1,23 @@
+x<-25
+hello <-"Hello WOrld"
+print(hello)
+hello
+print("How are you")
+help.start()
+
+x <-c(1,2,2,NA,5,10)
+x
+min(x,na.rm = TRUE)
+max(x,na.rm = TRUE)
+mean(x,na.rm = TRUE)
+y <- mean(x,na.rm = TRUE)
+y
+
+
+?min()
+
+summary(x)
+
+ls()
+
+rm("x")
